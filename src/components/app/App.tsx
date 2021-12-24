1f2a246b9c58faaf01dfd3e3e958127465bd6107
Brandon <BrandonPoole@live.ca>
2021-12-24 09:09:27 -0500

    update dependencies

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from 'components/routes/home/home';
 import PageNotFound from 'components/routes/404/404';
 
-export default () => {
-  return (
+const App = () =>  
+(
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route component={PageNotFound} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route element={<PageNotFound />} />
+      </Routes>
     </BrowserRouter>
-  );
-}
+)
 
+export default App
